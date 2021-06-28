4bee5052f89a587434abedea5a73ffb2657fb092
xuezzou <ilfam1999@gmail.com>
2021-06-27 23:19:55 -0400

    added media queries in react component and changed responsive order;

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useFirestore from '../hooks/useFirestore';
 import { motion } from 'framer-motion';
 
@@ -8,13 +8,27 @@ const sliceArr = (arr, moduleNum, remainder) => {
     })
 };
 
+
 const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore('images');
 
-  return (
-    <div class="row">
-      <div class="column">
-        {docs && sliceArr(docs, 4, 0).map(doc => (
+  // media query lists
+  const [isOneCol, setIsOneCol] = useState(window.matchMedia('(max-width: 600px)').matches);
+  const [isTwoCol, setIsTwoCol] = useState(window.matchMedia('(max-width: 800px)').matches);
+  useEffect(() => {
+    const handler = () => {
+      setIsOneCol(window.matchMedia('(max-width: 600px)').matches);
+      setIsTwoCol(window.matchMedia('(max-width: 800px)').matches);
+    }
+    window.addEventListener('resize', handler);
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handler);
+  }, []);
+
+  const getColHtml = (colNum, index) => {
+    return (
+      <div className="column">
+        {docs && sliceArr(docs, colNum, index).map(doc => (
           <motion.div className="img-wrap" key={doc.id} 
             layout
             whileHover={{ opacity: 1 }}s
@@ -27,50 +41,32 @@ const ImageGrid = ({ setSelectedImg }) => {
           </motion.div>
         ))}
       </div>
-      <div class="column">
-        {docs && sliceArr(docs, 4, 1).map(doc => (
-          <motion.div className="img-wrap" key={doc.id} 
-            layout
-            whileHover={{ opacity: 1 }}s
-            onClick={() => setSelectedImg(doc.url)}>
-            <motion.img src={doc.url} alt="uploaded pic"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            />
-          </motion.div>
-        ))}
+    )
+  };
+
+  if(isOneCol && isTwoCol) {
+    return (
+      <div className="row">
+        {getColHtml(1,0)}
       </div>
-      <div class="column">
-        {docs && sliceArr(docs, 4, 2).map(doc => (
-          <motion.div className="img-wrap" key={doc.id} 
-            layout
-            whileHover={{ opacity: 1 }}s
-            onClick={() => setSelectedImg(doc.url)}>
-            <motion.img src={doc.url} alt="uploaded pic"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            />
-          </motion.div>
-        ))}
+    ) 
+  } else if(isTwoCol) {
+    return (
+      <div className="row">
+        {getColHtml(2,0)}
+        {getColHtml(2,1)}
       </div>
-      <div class="column">
-        {docs && sliceArr(docs, 4, 3).map(doc => (
-          <motion.div className="img-wrap" key={doc.id} 
-            layout
-            whileHover={{ opacity: 1 }}s
-            onClick={() => setSelectedImg(doc.url)}>
-            <motion.img src={doc.url} alt="uploaded pic"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            />
-          </motion.div>
-        ))}
-      </div>    
-    </div>
-  )
+    ) 
+  } else {
+    return (
+      <div className="row">
+        {getColHtml(4,0)}
+        {getColHtml(4,1)}
+        {getColHtml(4,2)}
+        {getColHtml(4,3)}
+      </div>
+    ) 
+  }
 }
 
 export default ImageGrid;
