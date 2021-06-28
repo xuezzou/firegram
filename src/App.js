e36e3c41c1d0ac11b3e2c2a9a5072ffbf5c7535e
xuezzou <ilfam1999@gmail.com>
2021-06-28 15:24:38 -0400

    removed upload form label;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Title from './comps/Title';
-import UploadForm from './comps/UploadForm';
+// import UploadForm from './comps/UploadForm';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
 import Footer from './comps/Footer';
@@ -15,7 +15,7 @@ function App() {
       { selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
       )}
-      <UploadForm />
+      {/* <UploadForm /> */}
       <Footer />
     </div>
   );
